6b5d0fd2f0fb556fb86a35258f6d4338b31b514e
Alex-IA69 <A01635557@tec.mx>
2023-06-09 23:39:19 -0600

    primer push

--- client/src/elements/ActualizarReceta.js
+++ client/src/elements/ActualizarReceta.js
@@ -3,23 +3,30 @@ import axios from 'axios';
 
 const ActualizarReceta = ({ onSubmit }) => {
     
-    
+    function modificarReceta() {
+        var receta_id = document.getElementById("id").value;
+        var pasos_id = document.getElementById("id").value;
+        axios.patch('/receta/$(receta_id)', {'pasos': pasos_id});
+    }
 
   return (
     <section className="formulario-receta">
       <h2>Actualizar Receta</h2>
-      <form>
-        <label>
-            Id:
-            <input type="number"/>
-        </label>
-
-        <label>
-            Pasos:
-            <textarea/>
-        </label>
-        <button>Actualizar</button>
-      </form>
+      <form action="/receta" method="patch">
+            <ul>
+                <li>
+                    <label for="id">ID:</label>
+                    <input type="number" id="id" name="receta_id" />
+                </li>
+                <li>
+                    <label for="pasos">Pasos:</label>
+                    <input type="text" id="pasos" name="pasos_id" />
+                </li>
+                <li class="button">
+                    <button type="submit" onClick="modificarReceta"> Actualizar</button>
+                    </li>
+            </ul>
+        </form>
     </section>
   );
 };
